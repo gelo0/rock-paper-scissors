a90db0132c371872b73e5bc76116902b12c7d95e
Gelo <gelo0@o2.pl>
2021-11-19 18:35:07 +0100

    First working draft

--- myScript.js
+++ myScript.js
@@ -5,10 +5,10 @@ function computerPlay() {
         return "ROCK";
     }
     else if (random === 1) {
-        return "PAPER"
+        return "PAPER";
     }
     else {
-        return "SCISSORS"
+        return "SCISSORS";
     }
 }
 
@@ -16,7 +16,7 @@ function playerSelection() {
     const input = prompt("Choose ROCK, PAPER or SCISSORS: ");
     let playersChoice = input.toUpperCase();
     if (playersChoice !== 'ROCK' && playersChoice !== 'PAPER' && playersChoice !== 'SCISSORS') {
-        alert("You can only choose ROCK, PAPER or SCISSORS")
+        alert("You can only choose ROCK, PAPER or SCISSORS");
         return playerSelection();
     }
     else {
@@ -28,35 +28,41 @@ function playerSelection() {
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === computerSelection) {
-        console.log("DRAW");
+        return("DRAW");
     }
     else if (playerSelection === 'ROCK') {
         if (computerSelection === 'PAPER') {
-            console.log('You lose. PAPER beats ROCK');
+            ++computerScore;
+            return('You lose. PAPER beats ROCK');
             return ++computerScore;
         }
         else {
-            console.log('You win. ROCK beats SCISSORS');
+            ++playerScore;
+            return('You win. ROCK beats SCISSORS');
             return ++playerScore;
         }    
     }
     else if (playerSelection === 'PAPER') {
         if (computerSelection === 'SCISSORS') {
-            console.log('You lose. SCISSORS beats PAPER');
+            ++computerScore;
+            return('You lose. SCISSORS beats PAPER');
             return ++computerScore;
         }
         else {
-            console.log('You win. PAPER beats ROCK');
+            ++playerScore;
+            return('You win. PAPER beats ROCK');
             return ++playerScore;
         }
     }
     else if (playerSelection === 'SCISSORS') {
         if (computerSelection === 'ROCK') {
-            console.log('You lose. ROCK beats SCISSORS');
+            ++computerScore;
+            return('You lose. ROCK beats SCISSORS');
             return ++computerScore;
         }
         else {
-            console.log('You win. SCISSORS beats PAPER');
+            ++playerScore;
+            return('You win. SCISSORS beats PAPER');
             return ++playerScore;
         }
     }
@@ -64,19 +70,53 @@ function playRound(playerSelection, computerSelection) {
 
 let playerScore = 0;
 let computerScore = 0;
+let roundCount = 1;
 
 function game(){
     for (let i = 1; i < 6; i++){
-        console.log(`Round: ${i}`)
+        console.log(`Round: ${i}`);
         let round = playRound(playerSelection(), computerPlay());
-        //console.log(round)
-        console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+        console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
     }
     finalScore(playerScore, computerScore);
 }
 
 function finalScore(playerScore, computerScore) {
-    if (playerScore > computerScore) alert('YOU WIN!!!');
-    else if (playerScore < computerScore) alert('YOU LOSE!!!');
-    else alert('DRAW!!!');
+    if (playerScore > computerScore) return('END GAME. YOU WIN!!!');
+    else if (playerScore < computerScore) return('END GAME. YOU LOSE!!!');
+    else return('END GAME. DRAW!!!');
+}
+
+const gameStats = document.querySelector('#game-stats');
+const roundNumber = document.querySelector('#round-number');
+const buttons = document.querySelectorAll('button');
+
+printRound(roundCount);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (roundCount <= 5){
+            console.log(`'${button.id}'`);
+            ++roundCount;
+            let round = playRound(button.id, computerPlay());
+            printResult(round);
+            printStats(playerScore, computerScore);
+            if (roundCount > 5) printRound(finalScore(playerScore, computerScore))
+            else printRound(roundCount);
+        };
+    });
+});
+
+function printResult (string) {
+    const text = document.querySelector('#round-result');
+    text.textContent = string;
+}
+
+function printStats (playerScore, computerScore) {
+    gameStats.innerHTML = `Player Score: ${playerScore} 
+      <br>Computer Score: ${computerScore}`;
+}
+
+function printRound (round) {
+    roundNumber.textContent = `Round: ${round}`;
 }
